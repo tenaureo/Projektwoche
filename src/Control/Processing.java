0ff9cacbadffb295308ca167f8b1bf4593526153
Tenzin Thobten Leduk <50613123+tenaureo@users.noreply.github.com>
2020-10-02 00:15:55 +0200

    Update Processing.java

--- src/Control/Processing.java
+++ src/Control/Processing.java
@@ -2,19 +2,28 @@ package Control;
 
 import Model.SDAT;
 import Model.ESL;
+import com.sun.source.tree.Tree;
 
 import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
+
+
+
 
 /**
  * @author Vator AG
  * @version 1.0
  * @since 2020-September-30
  */
-
 public class Processing {
+
     private Map<String, ESL> eslMap;
     private Map<String, SDAT> sdatMap;
 
+    public Processing(){
+
+    }
     public Map<String, ESL> getEslMap() {
         return eslMap;
     }
@@ -31,12 +40,16 @@ public class Processing {
         this.sdatMap = sdatMap;
     }
 
-    public Processing(){
-
-
-    }
-
+    //sorts data from Hashmap by keys which are Timestamps, ranging from earliest to latest
     public void sortData(){
 
-    }
+            Processing pr = new Processing();
+
+            pr.getSdatMap();
+
+            TreeSet<String> keyset = (TreeSet<String>) sdatMap.keySet();
+
+            System.out.println(keyset);
+     }
+
 }
