63756a5ce40632c4ba2557b98053c422f2c6999b
Leo Oetterli <61965711+pheonix8@users.noreply.github.com>
2020-10-01 08:38:47 +0200

    Add files via upload

--- src/View/View.java
+++ src/View/View.java
@@ -3,6 +3,11 @@ package View;
 import Control.Processing;
 import Datahandler.DataHandler;
 import Model.SDAT;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 import model.ESL;
 
 import java.awt.*;
@@ -14,7 +19,7 @@ import java.util.Map;
  * @since 2020-September-30
  */
 
-public class View {
+public class View extends Application {
     private Map<String, SDAT> sdatMap;
     private Map<String, ESL> eslMap;
 
@@ -22,8 +27,15 @@ public class View {
     private Processing processing;
     private CardLayout cardLayout;
 
-    public View(){
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setTitle("Energieagentur Bünzli Stromzähler");
 
+        Parent root = FXMLLoader.load(getClass().getResource("FXML/StartUI.fxml"));
+        Scene scene = new Scene(root);
+
+        stage.setScene(scene);
+        stage.show();
     }
 
 }
