d62b2044ecd529dea4e3b7faae4810baa6651837
Vinicíus Leonárdo Navarra <viniciuslnavarra@outlook.com>
2020-10-01 08:59:44 +0200

    ESL done in DataHandler

--- src/Datahandler/DataHandler.java
+++ src/Datahandler/DataHandler.java
@@ -1,17 +1,20 @@
 package Datahandler;
 
-import model.ESL;
+import Model.SDAT;
+import Model.ESL;
 
+import java.util.ArrayList;
 import java.util.Map;
 
 /**
- * @author Moritz Meier
+ * @author Vator
  * @version 1.0
  * @since 2020-September-30
  */
 
 public class DataHandler {
 
+    private ESLXml eslXml;
     private  Map<String, SDAT> sdatMap;
     private Map<String, ESL> eslMap;
     private DataHandler dataHandler;
@@ -19,8 +22,19 @@ public class DataHandler {
     public void readSdat(){
 
     }
-    public void readESL(){
 
+    public void readESL(){
+        eslXml = new ESLXml();
+        for (TimePeriod timePeriod : eslXml.getTimePeriodList()) {
+            if (!eslMap.containsKey(timePeriod.getTime().toString())) {
+                for (Values value : timePeriod.getValuesList()) {
+                    String obis = value.getObis();
+                    if(obis.endsWith("1.8.1") || obis.endsWith("1.8.2")) {
+                        eslMap.put(timePeriod.getTime().toString(), new ESL(timePeriod.getTime(), obis.substring(obis.length()-6), value.getValue()));
+                    }
+                }
+            }
+        }
     }
 
     public void addSdat(){
